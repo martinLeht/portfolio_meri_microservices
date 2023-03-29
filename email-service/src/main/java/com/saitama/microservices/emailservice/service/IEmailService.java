f003f2e70b0c23b0f7d41d6e2c41b3e4f1df779e
martinLeht <martin.lehtomaa@gmail.com>
2023-03-29 16:50:25 +0300

    Removed comment-service and email-service - takes too much resources when deployed as dedicated services. comment-service logic is moved to portfolio-data-service and email-service is moved to authentication-service.

--- email-service/src/main/java/com/saitama/microservices/emailservice/service/IEmailService.java
+++
@@ -1,9 +0,0 @@
-package com.saitama.microservices.emailservice.service;
-
-import com.saitama.microservices.emailservice.dto.MessageDTO;
-
-public interface IEmailService {
-	
-	boolean sendMail(MessageDTO message);
-
-}
